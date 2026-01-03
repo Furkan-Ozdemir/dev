c0e8455b525d50629b535495759a0ab7a65c8436
Furkan Ozdemir <ozthefur@gmail.com>
2026-01-03 19:19:04 +0300

    input output

--- cpp/ilk.cpp
+++ cpp/ilk.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 
-int main(){
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number32" << std::endl;
+int main()
+{
+
+    std::string country;
+
+    std::cout << "Where do you live?" << std::endl;
+    std::cin >> country;
+
+    std::cout << "I've heard great things about " << country << ". I'd like to go there sometime." << std::endl;
+
     return 0;
 }
