4a60d60e35fd33afe218119832127c013e199319
Furkan Ozdemir <ozthefur@gmail.com>
2026-01-18 13:08:50 +0300

    chars

---
+++ cpp/variables and data types/chars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+    char value{65};
+    char char1{'f'};
+
+    std::cout << char1 << std::endl;
+    std::cout << static_cast<int>(value) << std::endl;
+
+    return 0;
+}
